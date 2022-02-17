06fea55e9a3164868f59281327630520846586c6
Ang Kosal <angkosal@gmail.com>
2022-02-17 17:12:12 +0700

    Upgrade laravel to version 9

--- database/migrations/2020_05_04_165749_create_order_items_table.php
+++ database/migrations/2020_05_04_165749_create_order_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrderItemsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -35,4 +35,4 @@ class CreateOrderItemsTable extends Migration
     {
         Schema::dropIfExists('order_items');
     }
-}
+};
