b70fb8ca70cda09c0c1f80b71311f9d9139cd487
Ang Kosal <angkosal@gmail.com>
2022-03-17 08:39:14 +0700

    Update default layout title

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>@yield('title')</title>
+  <title>@yield('title', config('app.name'))</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Ionicons -->
