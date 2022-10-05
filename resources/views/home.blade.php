3bfa3fb8bbf01844e1d4c21e525a3b416d3aca7c
weiliang79 <liang2010@hotmail.my>
2022-10-05 09:53:50 +0800

    comment route lead to error

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -59,7 +59,7 @@
               <div class="icon">
                 <i class="ion ion-person-add"></i>
               </div>
-              <a href="{{ route('customers.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{-- route('customers.index') --}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
