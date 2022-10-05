54b893549a0cd7db9458a0ce03c5fdb103b077f5
weiliang79 <liang2010@hotmail.my>
2022-10-05 11:39:40 +0800

    feat: order, detail, student model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,14 +2,57 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
+
+    use HasFactory, SoftDeletes;
+
+    const PAYMENT_PENDING = 1, PAYMENT_FAILURE = 2, PAYMENT_SUCCESS = 3;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
-        'customer_id',
-        'user_id'
+        'student_id',
+        'pick_up_start',
+        'pick_up_end',
+        'total_price',
+        'status',
+        'is_sandbox_order',
     ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'pick_up_start' => 'datetime',
+        'pick_up_end' => 'datetime',
+    ];
+
+    public function student(){
+        return $this->belongsTo(Student::class);
+    }
+
+    public function orderDetails(){
+        return $this->hasMany(OrderDetail::class);
+    }
+
+    public function getStatusString(){
+        switch($this->status){
+            case Order::PAYMENT_PENDING: return 'Payment Pending';
+            case Order::PAYMENT_FAILURE: return 'Payment Failure';
+            case Order::PAYMENT_SUCCESS: return 'Payment Success';
+            default: return 'Undefined';
+        }
+    }
 
     public function items()
     {
---
+++ app/Models/OrderDetail.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class OrderDetail extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'product_options',
+        'price',
+        'notes',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'product_options' => 'array',
+    ];
+
+    public function order(){
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
+
+}
