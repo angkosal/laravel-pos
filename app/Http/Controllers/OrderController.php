4da7f54e01c1bf827cee3d9998f3ef85ac8c3049
Wei Liang <liang2010@hotmail.my>
2022-10-08 21:57:31 +0800

    changes: remove comments and add orderBy

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -11,30 +11,13 @@ use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
-    public function index(Request $request) {
-        // $orders = new Order();
-        // if($request->start_date) {
-        //     $orders = $orders->where('created_at', '>=', $request->start_date);
-        // }
-        // if($request->end_date) {
-        //     $orders = $orders->where('created_at', '<=', $request->end_date . ' 23:59:59');
-        // }
-        // $orders = $orders->with(['items', 'payments', 'customer'])->latest()->paginate(10);
-
-        // $total = $orders->map(function($i) {
-        //     return $i->total();
-        // })->sum();
-        // $receivedAmount = $orders->map(function($i) {
-        //     return $i->receivedAmount();
-        // })->sum();
+    public function index() {
 
         $user = User::find(Auth::user()->id);
         $productIds = $user->store->products()->pluck('id')->toArray();
         $orders = Order::whereHas('orderDetails', function ($query) use ($productIds) {
             $query->whereIn('product_id', $productIds);
-        })->get();
-
-        //dd($orders);
+        })->orderBy('created_at', 'desc')->get();
 
         return view('orders.index', compact('orders'));
     }
@@ -54,28 +37,4 @@ class OrderController extends Controller
         return view('orders.details', compact('order', 'details', 'detailsOtherStore'));
     }
 
-    public function store(OrderStoreRequest $request)
-    {
-        $order = Order::create([
-            'customer_id' => $request->customer_id,
-            'user_id' => $request->user()->id,
-        ]);
-
-        $cart = $request->user()->cart()->get();
-        foreach ($cart as $item) {
-            $order->items()->create([
-                'price' => $item->price * $item->pivot->quantity,
-                'quantity' => $item->pivot->quantity,
-                'product_id' => $item->id,
-            ]);
-            $item->quantity = $item->quantity - $item->pivot->quantity;
-            $item->save();
-        }
-        $request->user()->cart()->detach();
-        $order->payments()->create([
-            'amount' => $request->amount,
-            'user_id' => $request->user()->id,
-        ]);
-        return 'success';
-    }
 }
