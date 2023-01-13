184bdb95690da83a30a4058ed9ebf90e038371db
Azhar Ibn Mostafiz <theazharul@gmail.com>
2023-01-13 21:57:27 +0600

    purhase_price and selling_price field added

---
+++ database/migrations/2023_01_13_155319_add_purhase_price_and_selling_price_in_product_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->decimal('purchase_price', 8, 2);
+            $table->decimal('selling_price', 8, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('product', function (Blueprint $table) {
+            //
+        });
+    }
+};
