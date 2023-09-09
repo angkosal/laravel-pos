7ccdc1fbe61f619436b95c1b4df7deca2ddb3ee4
Ang Kosal <angkosal@gmail.com>
2023-09-09 15:55:18 +0700

    Fixed product and customer can not delete #32

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -36,16 +36,13 @@
                     <td>{{$product->price}}</td>
                     <td>{{$product->quantity}}</td>
                     <td>
-                        <span
-                            class="right badge badge-{{ $product->status ? 'success' : 'danger' }}">{{$product->status ? 'Active' : 'Inactive'}}</span>
+                        <span class="right badge badge-{{ $product->status ? 'success' : 'danger' }}">{{$product->status ? 'Active' : 'Inactive'}}</span>
                     </td>
                     <td>{{$product->created_at}}</td>
                     <td>{{$product->updated_at}}</td>
                     <td>
-                        <a href="{{ route('products.edit', $product) }}" class="btn btn-primary"><i
-                                class="fas fa-edit"></i></a>
-                        <button class="btn btn-danger btn-delete" data-url="{{route('products.destroy', $product)}}"><i
-                                class="fas fa-trash"></i></button>
+                        <a href="{{ route('products.edit', $product) }}" class="btn btn-primary"><i class="fas fa-edit"></i></a>
+                        <button class="btn btn-danger btn-delete" data-url="{{route('products.destroy', $product)}}"><i class="fas fa-trash"></i></button>
                     </td>
                 </tr>
                 @endforeach
@@ -58,19 +55,19 @@
 
 @section('js')
 <script src="{{ asset('plugins/sweetalert2/sweetalert2.min.js') }}"></script>
-<script>
-    $(document).ready(function () {
-        $(document).on('click', '.btn-delete', function () {
-            $this = $(this);
+<script type="module">
+    $(document).ready(function() {
+        $(document).on('click', '.btn-delete', function() {
+            var $this = $(this);
             const swalWithBootstrapButtons = Swal.mixin({
                 customClass: {
                     confirmButton: 'btn btn-success',
                     cancelButton: 'btn btn-danger'
                 },
                 buttonsStyling: false
-                })
+            })
 
-                swalWithBootstrapButtons.fire({
+            swalWithBootstrapButtons.fire({
                 title: 'Are you sure?',
                 text: "Do you really want to delete this product?",
                 icon: 'warning',
@@ -78,10 +75,13 @@
                 confirmButtonText: 'Yes, delete it!',
                 cancelButtonText: 'No',
                 reverseButtons: true
-                }).then((result) => {
+            }).then((result) => {
                 if (result.value) {
-                    $.post($this.data('url'), {_method: 'DELETE', _token: '{{csrf_token()}}'}, function (res) {
-                        $this.closest('tr').fadeOut(500, function () {
+                    $.post($this.data('url'), {
+                        _method: 'DELETE',
+                        _token: '{{csrf_token()}}'
+                    }, function(res) {
+                        $this.closest('tr').fadeOut(500, function() {
                             $(this).remove();
                         })
                     })
