65897944a6cc8e987e9c9873ae4ec23b9d208891
Ang Kosal <angkosal@gmail.com>
2024-08-17 10:10:14 +0700

    Update copyright

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>{{ __('common.Version') }}</b> 3.0.3
+        <b>{{ __('common.Version') }}</b> 3.0.3
     </div>
-    <strong>{{ __('common.Copyright') }}Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> {{ __('common.all_reserved') }}
-  </footer>
+    <strong>{{ __('common.Copyright') }} &copy; {{date('Y')}} <a href="http://github.com/angkosal">Ang Kosal</a>.</strong> {{ __('common.all_reserved') }}
+</footer>
