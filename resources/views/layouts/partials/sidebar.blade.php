b100c5a38aca84f6d74e499d2f96d11215ea4b45
Ang Kosal <angkosal@gmail.com>
2025-02-11 15:01:13 +0700

    Hide purchase menu

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -40,12 +40,13 @@
                         <p>{{ __('cart.title') }}</p>
                     </a>
                 </li>
+                {{--
                 <li class="nav-item has-treeview">
                     <a href="#" class="nav-link {{ activeSegment('Purchase') }}">
                         <i class="nav-icon fas fa-cart-plus"></i>
                         <p>{{ __('Purchase') }}</p>
                     </a>
-                </li>
+                </li>--}}
                 <li class="nav-item has-treeview">
                     <a href="{{ route('orders.index') }}" class="nav-link {{ activeSegment('orders') }}">
                         <i class="nav-icon fas fa-cart-plus"></i>
