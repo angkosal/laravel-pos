b4f38e5d118f980f58fc0f02997bd0d0a680f302
Ang Kosal <angkosal@gmail.com>
2025-11-13 16:54:21 +0700

    Prepare sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -9,16 +9,6 @@
 
     <!-- Sidebar -->
     <div class="sidebar">
-        <!-- Sidebar user (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="{{ auth()->user()->getAvatar() }}" class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
-                <a href="#" class="d-block">{{ auth()->user()->getFullname() }}</a>
-            </div>
-        </div>
-
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
@@ -38,14 +28,32 @@
                     </a>
                 </li>
 
+                <li class="nav-header">{{ __('Sales') }}</li>
                 <!-- POS Cart -->
                 <li class="nav-item">
                     <a href="{{ route('cart.index') }}" class="nav-link {{ activeSegment('cart') }}">
                         <i class="nav-icon fas fa-cart-plus"></i>
-                        <p>{{ __('cart.title') }}</p>
+                        <p>{{ __('POS') }}</p>
                     </a>
                 </li>
 
+                <!-- Orders -->
+                <li class="nav-item">
+                    <a href="{{ route('orders.index') }}" class="nav-link {{ activeSegment('orders') }}">
+                        <i class="nav-icon fas fa-shopping-cart"></i>
+                        <p>{{ __('Order List') }}</p>
+                    </a>
+                </li>
+
+                <!-- Customers -->
+                <li class="nav-item">
+                    <a href="{{ route('customers.index') }}" class="nav-link {{ activeSegment('customers') }}">
+                        <i class="nav-icon fas fa-users"></i>
+                        <p>{{ __('customer.title') }}</p>
+                    </a>
+                </li>
+
+                <li class="nav-header">{{ __('Purchases') }}</li>
                 <!-- Purchases (Dropdown) -->
                 <li class="nav-item {{ request()->routeIs('purchases.*') ? 'menu-open' : '' }}">
                     <a href="#" class="nav-link {{ activeSegment('purchases') }}">
@@ -70,23 +78,6 @@
                         </li>
                     </ul>
                 </li>
-
-                <!-- Orders -->
-                <li class="nav-item">
-                    <a href="{{ route('orders.index') }}" class="nav-link {{ activeSegment('orders') }}">
-                        <i class="nav-icon fas fa-shopping-cart"></i>
-                        <p>{{ __('order.title') }}</p>
-                    </a>
-                </li>
-
-                <!-- Customers -->
-                <li class="nav-item">
-                    <a href="{{ route('customers.index') }}" class="nav-link {{ activeSegment('customers') }}">
-                        <i class="nav-icon fas fa-users"></i>
-                        <p>{{ __('customer.title') }}</p>
-                    </a>
-                </li>
-
                 <!-- Suppliers -->
                 <li class="nav-item">
                     <a href="{{ route('suppliers.index') }}" class="nav-link {{ activeSegment('suppliers') }}">
@@ -95,6 +86,7 @@
                     </a>
                 </li>
 
+                <li class="nav-header">{{ __('Extra') }}</li>
                 <!-- Settings -->
                 <li class="nav-item">
                     <a href="{{ route('settings.index') }}" class="nav-link {{ activeSegment('settings') }}">
