34a47a5a2cc71736449a0e965e270bbac3908c12
Ang Kosal <angkosal@gmail.com>
2025-11-13 07:54:40 +0700

    Down remove purchase_price column

--- database/migrations/2025_11_12_103307_add_purchase_price_to_products.php
+++ database/migrations/2025_11_12_103307_add_purchase_price_to_products.php
@@ -24,7 +24,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->dropColumn('purchase_price');
         });
     }
 };
